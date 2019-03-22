a12ff5af05c1e61489e9ace46096346e13dd3c1d
Joe Madden <obj63mc@gmail.com>
2019-03-22 15:52:13 -0500

    Initial Files and Setup of MySQLi SSL Connection
    This is the initial commit for this module

---
+++ README.md
@@ -0,0 +1,13 @@
+# SilverStripe MySQLi SSL Connector
+
+SilverStripe module to allow MySQLi SSL connection with CA certificate only
+
+# Setup -
+
+1. Install Module
+        composer require obj63mc/silverstripe-mysqlisslconnector
+2. Place your CA certificate bundle in the root of your project folder '/' eg. $_SERVER['DOCUMENT_ROOT']
+3. Add an Environment variable for your CA filename eg.
+        SSL_CA_FILENAME='rds-combined-ca-bundle.pem'
+
+Once setup your DB will now be under an SSL Connection.
---
+++ src/Connect/MySQLiSSLConnector.php
@@ -0,0 +1,57 @@
+<?php
+namespace Obj63mc\ORM\Connect;
+
+use SilverStripe\ORM\Connect\MySQLiConnector;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Environment;
+use SilverStripe\ORM\Connect\MySQLDatabase;
+
+class MySQLiSSLConnector extends MySQLiConnector
+{
+
+    public function connect($parameters, $selectDB = false)
+    {
+        // Normally $selectDB is set to false by the MySQLDatabase controller, as per convention
+        $selectedDB = ($selectDB && !empty($parameters['database'])) ? $parameters['database'] : null;
+
+        // Connection charset and collation
+        $connCharset = Config::inst()->get(MySQLDatabase::class, 'connection_charset');
+        $connCollation = Config::inst()->get(MySQLDatabase::class, 'connection_collation');
+
+        $this->dbConn = mysqli_init();
+
+        //Custom SSL Config for RDS PEM only.
+        $this->dbConn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+        $this->dbConn->ssl_set(NULL, NULL, $_SERVER['DOCUMENT_ROOT'].'/'.Environment::getEnv('SSL_CA_FILENAME'), NULL, NULL);
+
+        $this->dbConn->real_connect(
+            $parameters['server'],
+            $parameters['username'],
+            $parameters['password'],
+            $selectedDB,
+            !empty($parameters['port']) ? $parameters['port'] : ini_get("mysqli.default_port")
+        );
+
+        if ($this->dbConn->connect_error) {
+            $this->databaseError("Couldn't connect to MySQL database | " . $this->dbConn->connect_error);
+        }
+
+        // Set charset and collation if given and not null. Can explicitly set to empty string to omit
+        $charset = isset($parameters['charset'])
+                ? $parameters['charset']
+                : $connCharset;
+
+        if (!empty($charset)) {
+            $this->dbConn->set_charset($charset);
+        }
+
+        $collation = isset($parameters['collation'])
+            ? $parameters['collation']
+            : $connCollation;
+
+        if (!empty($collation)) {
+            $this->dbConn->query("SET collation_connection = {$collation}");
+        }
+    }
+
+}
